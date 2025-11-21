fd7ec1d9e98fd79e7cac7aa47518e0006d02c338
carline41593-spec <carline41593@gmail.com>
2025-11-20 19:14:10 -0500

    Create page.tsx

---
+++ app/library/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen p-12 pl-96">
+      <h1 className="text-6xl font-bold bg-gradient-to-r from-pink-400 to-yellow-400 bg-clip-text text-transparent">
+        My Library
+      </h1>
+      <p className="text-xl text-gray-400 mt-6">All your generated images & audio</p>
+    </div>
+  );
+}
